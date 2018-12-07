6628fcd0c819c0e18c2c527af09d4984771cfcb1
matus258 <matusalem879@gmail.com>
2018-12-07 13:50:09 -0200

    Changes to be committed: 	new file:   .gitignore 	modified:   Java.java 	new file:   README

--- Java.java
+++ Java.java
@@ -1,5 +1,8 @@
 public class TreinamentoGit {
   public static void main (String[] args){
     system.out.println("Ola Mundo");
+    system.out.println("Modifiquei o Arquivo");
+    system.out.print("Modificado depois do git add");
+    system.out.print("Alterando para o git diff");
   }
 }
