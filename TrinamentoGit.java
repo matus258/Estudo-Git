de9cc085dfadba7ffa3b3f360005e6c19f108f03
matus258 <matusalem879@gmail.com>
2018-12-14 09:14:29 -0200

    teste

--- TrinamentoGit.java
+++ TrinamentoGit.java
@@ -2,7 +2,7 @@ public class TreinamentoGit {
   public static void main (String[] args){
   
     System.out.print("Modificado depois do git add");
-    System.out.print("Alterando para o gi");
+    System.out.print("Alterando para o git diff");
     system.out.print("Modificado depois do git add");
     system.out.print("Alterando para o git diff");
     int v;
