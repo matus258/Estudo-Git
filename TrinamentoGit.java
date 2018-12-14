3a0c260cee682465ee2957922020094c6fff3f62
matus258 <matusalem879@gmail.com>
2018-12-14 07:20:01 -0200

    Dois S maisculos do System

--- TrinamentoGit.java
+++ TrinamentoGit.java
@@ -1,7 +1,7 @@
 public class TreinamentoGit {
   public static void main (String[] args){
-    system.out.println("Ola Mundo");
-    system.out.println("Modifiquei o Arquivo");
+    System.out.println("Ola Mundo");
+    System.out.println("Modifiquei o Arquivo");
     system.out.print("Modificado depois do git add");
     system.out.print("Alterando para o git diff");
   }
