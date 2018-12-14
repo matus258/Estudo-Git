77920bbc55b78adbe1522258056076c6e533e7b8
matus258 <matusalem879@gmail.com>
2018-12-14 08:52:10 -0200

    ramo novo

--- TrinamentoGit.java
+++ TrinamentoGit.java
@@ -1,14 +1,11 @@
 public class TreinamentoGit {
   public static void main (String[] args){
-    System.out.println("Ola Mundo");
-    System.out.println("Modifiquei o Arquivo");
-<<<<<<< HEAD
+  
     System.out.print("Modificado depois do git add");
-    System.out.print("Alterando para o git diff");
-=======
+    System.out.print("Alterando para o gi");
     system.out.print("Modificado depois do git add");
     system.out.print("Alterando para o git diff");
     int v;
->>>>>>> variavel v
+
   }
 }
