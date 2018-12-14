86ea3ef93699a6be0e2e536d1c1470f69b49f65b
matus258 <matusalem879@gmail.com>
2018-12-14 10:27:21 -0200

    ms

--- TreinamentoGit.java
+++ TreinamentoGit.java
@@ -6,8 +6,8 @@ public class TreinamentoGit {
     System.out.print("Modificado depois do git add");
     System.out.print("Alterando para o git diff");
     int v;
-    int j;
-    double k;
+    int jas;
+    double kam;
     String ma;
     int re;
 
