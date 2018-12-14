26f07fa0dacea157f6dfe900d0955fb98507555b
matus258 <matusalem879@gmail.com>
2018-12-14 08:07:47 -0200

    variavel v

--- TrinamentoGit.java
+++ TrinamentoGit.java
@@ -2,7 +2,13 @@ public class TreinamentoGit {
   public static void main (String[] args){
     System.out.println("Ola Mundo");
     System.out.println("Modifiquei o Arquivo");
+<<<<<<< HEAD
     System.out.print("Modificado depois do git add");
     System.out.print("Alterando para o git diff");
+=======
+    system.out.print("Modificado depois do git add");
+    system.out.print("Alterando para o git diff");
+    int v;
+>>>>>>> variavel v
   }
 }
